02ea0666dfe05326bcb78372e91c47a30e6f6c15
Ruby <robabenyoussef@gmail.com>
2025-07-28 12:54:27 +0100

    updatee

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatNativeDateModule } from '@angular/material/core'; // ou MatMomentDateModule
+import { FormsModule } from '@angular/forms'; // déjà présent normalement
+import { NgModule } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatNativeDateModule
+  ],
   templateUrl: './app.html',
   styleUrls: ['./app.css']
 })
